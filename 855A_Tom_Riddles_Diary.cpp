f560d4f0cc9578cb6caee26a3bf2833f225cae1f
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-08 05:53:00 +0600

    Sync Codeforces: 855A - Tom Riddle's Diary [C++17 (GCC 7-32)]

---
+++ 855A_Tom_Riddles_Diary.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+ 
+    unordered_map<string, bool> a;
+ 
+    for (int i = 0; i < t; i++) {
+        string name;
+        cin >> name;
+ 
+        if (a[name]) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+            a[name] = true;
+        }
+    }
+ 
+    return 0;
+}
