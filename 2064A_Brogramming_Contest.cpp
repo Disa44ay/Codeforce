be4368d94028c8328e3ef90f103079d052d44f9f
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-08 05:52:53 +0600

    Sync Codeforces: 2064A - Brogramming Contest [C++17 (GCC 7-32)]

---
+++ 2064A_Brogramming_Contest.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+ 
+        int count = 0;
+        bool has_ones = false;
+ 
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                has_ones = true;
+            }
+            if (i < n - 1) {
+                if (s[i] == '1' && s[i + 1] == '0') {
+                    count++;
+                }
+            }
+        }
+ 
+        if (!has_ones) {
+            cout << "0\n";
+        } else {
+            if (s[n - 1] == '1') {
+                cout << 2 * count + 1 << "\n";
+            } else {
+                cout << 2 * count << "\n";
+            }
+        }
+    }
+    return 0;
+}
