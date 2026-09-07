827b9b7fdc21fe0c3876e289968d5ff5faacb674
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-08 05:52:39 +0600

    Sync Codeforces: 1878A - How Much Does Daytona Cost? [C++17 (GCC 7-32)]

---
+++ 1878A_How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n , k;
+        cin >> n >> k;
+        int a[n], count = 0;
+ 
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            if(a[i] == k){
+                count++;
+            }
+        }
+ 
+        if(count > 0){
+            cout << "YES\n";
+        }else{
+            cout << "NO\n";
+        }
+ 
+    }
+ 
+    return 0;
+}
