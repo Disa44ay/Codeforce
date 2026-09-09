ace97992884415ee12a4130654d8f43319230230
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-09 21:47:11 +0600

    Add Don't Try to Count [A] from Codeforces

---
+++ 1881/A - Don't Try to Count/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        string x, s;
+        cin >> x >> s;
+ 
+        for (int i = 0; i < 6; i++) {
+            if (x.find(s) != string::npos) {
+                cout << i << endl;
+                goto exit;
+            }
+            x += x;
+        }
+        cout << -1 << endl;
+    exit:;
+    }
+    return 0;
+}
