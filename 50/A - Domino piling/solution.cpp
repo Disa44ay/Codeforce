508c6a31149693c20d7d48de1c42fc615e16c05b
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-09 21:37:52 +0600

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n, a, s = 2, z;
+ 
+    scanf("%d%d", &m, &n);
+ 
+    a = m * n;
+    z = a / s;
+ 
+        printf("%d", z);
+ 
+}
