0b59563283ca3ec6e836f329b3d71cc5e35cdd87
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-09 21:42:42 +0600

    Add Closest Point [A] from Codeforces

---
+++ 2004/A - Closest Point/solution.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+ 
+        for(int i=0; i<n ; i++)
+        {
+            cin>>arr[i];
+        }
+ 
+        if(n>2)
+        {
+            cout<<"NO
+";
+        }
+        else if(n==2 && arr[0]==arr[1]-1)
+        {
+            cout<<"NO
+";
+        }
+        else
+        {
+            cout<<"YES
+";
+        }
+    }
+ 
+ 
+}
