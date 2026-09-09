7367f67966475d4b93a55d712f9350e6b769b06b
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-09 21:45:09 +0600

    Add Two Frogs [A] from Codeforces

---
+++ 2055/A - Two Frogs/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;cin >> t;
+    while(t--){
+        int n, a, b;
+        cin >> n >> a >> b;
+ 
+        if((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && b % 2 != 0)){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
