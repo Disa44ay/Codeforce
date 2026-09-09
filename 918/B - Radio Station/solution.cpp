728cf0f677135f1a57728bb7c0aef6a319bce337
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-09 21:48:48 +0600

    Add Radio Station [B] from Codeforces

---
+++ 918/B - Radio Station/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int n, m;
+    cin >> n >> m;
+ 
+    unordered_map<string, string> server_map;
+ 
+    for (int i = 0; i < n; i++) {
+        string name, ip;
+        cin >> name >> ip;
+        server_map[ip] = name;
+    }
+ 
+    for (int i = 0; i < m; i++) {
+        string command, ip;
+        cin >> command >> ip;
+ 
+        ip.pop_back();
+        cout << command << " " << ip << "; #" << server_map[ip] << "
+";
+    }
+ 
+    return 0;
+}
