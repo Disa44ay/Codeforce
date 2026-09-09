e17e338cabb14a8db2f3c448f66fe4ab8a6b110e
Abdullah Sayeed <158608223+Disa44ay@users.noreply.github.com>
2026-09-09 21:49:35 +0600

    Add Not Quite a Palindromic String [B] from Codeforces

---
+++ 2114/B - Not Quite a Palindromic String/solution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string bin;
+        cin >> bin;
+ 
+        int o = 0, z = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(bin[i] == '0')
+                z++;
+            else
+                o++;
+        }
+ 
+        int pc = 0;
+        while(pc != k)
+        {
+            if(o >= z && o >= 2)
+            {
+                o -= 2;
+                pc++;
+            }
+            else if(z >= o && z >= 2)
+            {
+                z -= 2;
+                pc++;
+            }
+            else
+            {
+                break;
+            }
+        }
+ 
+        if(o == z && pc == k)
+        {
+            cout << "YES
+";
+        }
+        else
+        {
+            cout << "NO
+";
+        }
+    }
+ 
+    return 0;
+}
